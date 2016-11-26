4f4e65537d807745e37c9b2b134e0f524f2c6d38
dementey <dementey@bitbucket.org>
2016-11-26 13:50:46 +0300

    Advanced Bootstrap theme build

---
+++ advanced-bootstrap/source/index.js
@@ -0,0 +1,5 @@
+require('motion-ui/dist/motion-ui.css');
+require('./scss/app.scss');
+window.jQuery = require('jquery/dist/jquery.min.js');
+require('foundation/dist/foundation.min.js');
+require('motion-ui/dist/motion-ui.js');
